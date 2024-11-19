81c65d07b29c1d61f8c984349a33e8e0ff22d9a1
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-11-19 13:28:00 +1100

    add training script

--- train.sh
+++ train.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-bash train_vit.sh dinov2_vits_exp 10
-bash train_vit.sh dinov2_vits_exp 5
+# bash train_vit.sh dinov2_vits_exp 10
+# bash train_vit.sh dinov2_vits_exp 5
 
-bash train_vit.sh dinov2_vitb_exp 10
-bash train_vit.sh dinov2_vitb_exp 5
+# bash train_vit.sh dinov2_vitb_exp 10
+# bash train_vit.sh dinov2_vitb_exp 5
+
+bash train_vit_others.sh dinov2_vitb_exp 5 cars 196
+bash train_vit_others.sh dinov2_vitb_exp 3 cars 196
+bash train_vit_others.sh dinov2_vits_exp 5 cars 196
+bash train_vit_others.sh dinov2_vits_exp 3 cars 196
